b4d8a0210fe025c74052d3bc1cefcc4ca340a324
handal95 <45425838+handal95@users.noreply.github.com>
2020-11-13 00:20:15 +0900

    [handpose] sameple code

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,29 +1,35 @@
 'use strict';
 
 // On this codelab, you will be streaming only video (video: true).
-const mediaStreamConstraints = {
-    video: true,
-};
-
+const mediaStreamConstraints = { video: true, };
 // Video element where stream will be placed.
 const localVideo = document.querySelector('video');
-
 // Local stream that will be reproduced on the video.
 let localStream;
-
 // Handles success by adding the MediaStream to the video element.
 function gotLocalMediaStream(mediaStream) {
     localStream = mediaStream;
     localVideo.srcObject = mediaStream;
 }
-
 // Handles error by logging a message to the console with the error message.
 function handleLocalMediaStreamError(error) {
     console.log('navigator.getUserMedia error: ', error);
 }
-
 // Initializes media stream.
 navigator.mediaDevices
     .getUserMedia(mediaStreamConstraints)
     .then(gotLocalMediaStream)
     .catch(handleLocalMediaStreamError);
+
+const handpose = require('@tensorflow-models/handpose');
+require('@tensorflow/tfjs-backend-webgl');
+
+async function main(){
+    const model = await handpose.load();
+    const video = document.querySelector("video_id");
+    const hands = await model.estimateHands(video);
+    
+    hands.forEach(hand => console.log(hand.landmarks));
+}
+
+main()
