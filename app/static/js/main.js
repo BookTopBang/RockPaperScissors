92366dabccd5d034c612fe28b937710b100e0002
handal95 <45425838+handal95@users.noreply.github.com>
2020-11-15 23:07:49 +0900

    handpose log

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,5 +1,3 @@
-'use strict';
-
 // On this codelab, you will be streaming only video (video: true).
 const mediaStreamConstraints = { video: true, };
 // Video element where stream will be placed.
@@ -21,15 +19,47 @@ navigator.mediaDevices
     .then(gotLocalMediaStream)
     .catch(handleLocalMediaStreamError);
 
-const handpose = require('@tensorflow-models/handpose');
-require('@tensorflow/tfjs-backend-webgl');
+
+async function estimate() {
+    // Load the MediaPipe handpose model assets.
+    const model = await handpose.load();
+
+    // Pass in a video stream to the model to obtain 
+    // a prediction from the MediaPipe graph.
+    const video = localVideo;
+    const predictions = await model.estimateHands(video);
+
+    // Each hand object contains a `landmarks` property,
+    // which is an array of 21 3-D landmarks.
+    // predictions.forEach(hand => console.log(hand.landmarks));
+
+    if (predictions.length > 0) {
+        for (let i = 0; i < predictions.length; i++) {
+            const keypoints = predictions[i].landmarks;
+            
+            // Log hand keypoints.
+            for (let i = 0; i < keypoints.length; i++) {
+                const [x, y, z] = keypoints[i];
+                console.log(`Keypoint ${i}: [${x}, ${y}, ${z}]`);
+            }
+        }
+    }
+
+}
+
+const sleep = (ms) => {
+    console.log(`time`)
+    return new Promise(resolve=>{
+        setTimeout(resolve,ms)
+    })
+}
+
 
 async function main(){
-    const model = await handpose.load();
-    const video = document.querySelector("video_id");
-    const hands = await model.estimateHands(video);
-    
-    hands.forEach(hand => console.log(hand.landmarks));
+    for(let i = 0; i < 5; i++){
+        estimate()
+        await sleep(5000);
+    }
 }
 
 main()
