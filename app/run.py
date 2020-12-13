e0fb3697e3f6878cb5dee7777123b4f79c31907c
handal95 <45425838+handal95@users.noreply.github.com>
2020-12-13 16:31:36 +0900

    [canvas] opponent canvas

--- app/run.py
+++ app/run.py
@@ -4,6 +4,7 @@ from flask import render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def main():
     """
@@ -12,14 +13,15 @@ def main():
     templates를 활용한 index.html을 확인할 수 있다.
     """
     # render_template은 templates/를 기본 경로로 갖는다.
-    
+
     html = 'index.html'
     return render_template(html)
+
 
 @app.route('/<mode>')
 def hello(mode=None):
     """
-    http://127.0.0.1:80/<mode> 로 접근하면 
+    http://127.0.0.1:80/<mode> 로 접근하면
     로컬호스트 환경의 80 포트의 '/'경로(http://127.0.0.1:80/)에서
     templates를 활용한 index.html을 확인할 수 있다.
     params :: debug
@@ -29,4 +31,5 @@ def hello(mode=None):
     html = 'index.html'
     return render_template(html, mode=mode)
 
-app.run(host = "0.0.0.0", port = 80, debug=True)
+
+app.run(host="0.0.0.0", port=80, debug=True)
